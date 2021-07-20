3ac0d5a6a237eb72e442917af3bbac4f38229861
Marc Cañigueral <m.canigueral@resourcefully.nl>
2021-07-20 10:39:07 +0200

    New get_demand function

--- R/demand.R
+++ R/demand.R
@@ -1,55 +1,48 @@
 
 # Demand ------------------------------------------------------------------
 
-#' Obtain demand from a starting dttm value and certain duration interval
-#'
-#' @param sessions tibble, sessions data set in standard format marked by `{evprof}` package
-#' @param timeslot datetime, time slot of the requested demand
-#' @param by character, being 'Profile' or 'Session'. When `by='Profile'` each column corresponds to an EV user profile.
-#'
-#' @return tibble
-#'
-#' @importFrom dplyr %>% filter group_by summarise mutate sym
-#' @importFrom tidyr spread
-#' @importFrom rlang .data
-#'
-#' @details This function is only valid if charging start/end times of sessions are aligned to a specific time-interval
-#'
-get_interval_demand <- function(sessions, timeslot, by = c("Profile", "Session")) {
-  sessions %>%
-    filter(.data$ChargingStartDateTime <= timeslot, timeslot < .data$ChargingEndDateTime) %>%
-    group_by(!!sym(by)) %>%
-    summarise(Power = sum(.data$Power)) %>%
-    spread(!!sym(by), .data$Power) %>%
-    mutate(datetime = timeslot)
-}
-
-
 #' Obtain timeseries demand from sessions dataset
 #'
 #' @param sessions tibble, sessions data set in standard format marked by `{evprof}` package
 #' @param dttm_seq sequence of datetime values that will be the datetime variable of the returned time-series data frame
 #' @param by character, being 'Profile' or 'Session'. When `by='Profile'` each column corresponds to an EV user profile.
+#' @param resolution integer, time resolution (in minutes) of the sessions datetime variables. If `dttm_seq` is defined this parameter is ignored.
 #'
 #' @return tibble
 #' @export
 #'
-#' @importFrom dplyr %>% left_join select everything rename mutate_if
-#' @importFrom lubridate is.timepoint round_date
-#' @importFrom purrr map2_dfr
+#' @importFrom dplyr left_join tibble sym
+#' @importFrom rlang .data
+#' @importFrom tidyr pivot_wider
+#' @importFrom purrr map_dfr
+#' @importFrom lubridate floor_date days
 #'
-get_demand <- function(sessions, dttm_seq, by = "Profile") {
-  interval_mins <- as.integer(as.numeric(dttm_seq[2] - dttm_seq[1], unit = 'hours')*60)
-
-  sessions <- sessions %>%
-    mutate_if(is.timepoint, round_date, paste(interval_mins, "minutes"))
-
-  demand <- map_dfr(dttm_seq, ~get_interval_demand(sessions, .x, by))
-
-  demand_df <- tibble(datetime = dttm_seq) %>% left_join(demand, by = "datetime")
-  demand_df <- replace(demand_df, is.na(demand_df), 0)
-
-  return( demand_df )
+#' @details This function is only valid if charging start/end times of sessions are aligned to a specific time-interval.
+#' For this purpose use `approximate_sessions` function.
+#'
+get_demand <- function(sessions, dttm_seq = NULL, by = "Profile", resolution = 15) {
+  if (is.null(dttm_seq)) {
+    dttm_seq <- seq.POSIXt(
+      from = floor_date(min(sessions$ConnectionStartDateTime)),
+      to = floor_date(max(sessions$ConnectionEndDateTime))+days(1),
+      by = paste(resolution, 'min')
+    )
+  }
+  demand <- left_join(
+    tibble(datetime = dttm_seq),
+    map_dfr(dttm_seq, ~ get_sessions_interval_demand(sessions, .x, by, normalized)) %>%
+      pivot_wider(names_from = !!sym(by), values_from = .data$Power, values_fill = 0),
+    by = 'datetime'
+  )
+  return( replace(demand, is.na(demand), 0) )
 }
 
+
+get_interval_demand <- function(sessions, timeslot, by) {
+  sessions %>%
+    dplyr::filter(.data$ChargingStartDateTime <= timeslot, timeslot < .data$ChargingEndDateTime) %>%
+    dplyr::group_by(!!dplyr::sym(by)) %>%
+    dplyr::summarise(Power = sum(.data$Power)) %>%
+    dplyr::mutate(datetime = timeslot)
+}
 
